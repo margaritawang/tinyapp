c999b01135adc0e4ebb499026bdf492c16e28f96
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-12-08 22:19:55 +0000

    method override

--- express_server.js
+++ express_server.js
@@ -3,13 +3,16 @@ var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 const bcrypt = require('bcrypt');
 var cookieSession = require('cookie-session');
+var methodOverride = require('method-override');
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(methodOverride('_method'));
+
 app.use(cookieSession({
   name: 'session',
-  keys: ['yuyumeer']
+  keys: ['key']
 }));
 
 app.set('view engine', 'ejs');
@@ -76,7 +79,13 @@ function urlsForUser(id) {
 }
 
 app.get("/", (req, res) => {
-  res.redirect('/urls');
+  //if user is logged in, redirect to /urls
+  if (checkLoginStatus(req)) {
+    res.redirect('/urls');
+  } else {
+  //if user is not logged in,redirect to /login
+    res.redirect('/login');
+  }
 });
 
 app.get('/urls.json', (req, res) => {
@@ -85,21 +94,35 @@ app.get('/urls.json', (req, res) => {
 
 
 app.get('/urls', (req, res) => {
-  let templateVars = {
+//   if user is logged in returns HTML with: the site header, a list (or table) of URLs the user has created, each list item containing:
+// a short URL, the short URL's matching long URL, an edit button which makes a GET request to /urls/:id
+// a delete button which makes a POST request to /urls/:id/delete
+// a link to "Create a New Short Link" which makes a GET request to /urls/new
+  if (checkLoginStatus(req)) {
+    let templateVars = {
     user_id: req.session.userID,
-    userDB: urlsForUser(req.session.userID)
+    userDB: urlsForUser(req.session.userID),
+    email: users[req.session.userID].email
+    }
+    res.render('urls_index', templateVars);
+  } else {
+// if user is not logged in returns HTML with a relevant error message
+    let templateVars = {
+    user_id: req.session.userID,
+    userDB: urlsForUser(req.session.userID),
+    }
+    res.render('urls_index', templateVars);
   }
-  // console.log(?);
-  res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+
+  if (checkLoginStatus(req)) {
   let templateVars = {
     users: users,
     user_id: req.session.userID,
-  };
-
-  if (checkLoginStatus(req)) {
+    email: users[req.session.userID].email
+    };
   res.render("urls_new",templateVars);
   } else {
     res.redirect('/login');
@@ -118,54 +141,65 @@ app.post("/urls", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL].longURL;
-  res.redirect(longURL);
+  if (!urlDatabase[shortURL]) {
+    res.send("URL doesn't exist!");
+  } else {
+    let longURL = urlDatabase[shortURL].longURL;
+    res.redirect(longURL);
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
   var shortURL = req.params.id;
   var userDB = urlsForUser(req.session.userID);
   console.log(userDB);
-  let templateVars = {
-   shortURL:  shortURL,
-   longURL: userDB[shortURL],
-   user_id: req.session.userID
-  }
 
-  if (userDB[req.params.id] === undefined) {
-    res.send('Please log in first!')
+  if (!req.session.userID) {
+    res.send('Please log in first!');
+  } else if (userDB[req.params.id] === undefined) {
+    res.send('No access to the URL!')
   } else {
+    let templateVars = {
+      shortURL:  shortURL,
+      longURL: userDB[shortURL],
+      user_id: req.session.userID,
+      email: users[req.session.userID].email
+    }
     res.render("urls_show", templateVars);
   }
 });
 
-app.post("/urls/:id", (req, res) => {
+app.put("/urls/:id", (req, res) => {
   let shortURL = req.params.id;
   let longURL = req.body.longURL;
   if (!shortURL || !longURL) {
     res.redirect('/urls');
-  }
+  } else
 
   urlDatabase[shortURL].longURL = longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.post('/urls/:id/delete', (req, res) => {
+app.delete('/urls/:id', (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
 app.get('/login', (req, res) => {
+  if (checkLoginStatus(req)) {
+    res.redirect('/urls');
+  } else {
   res.render('login');
+  }
 });
 
 app.post('/login', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  //  if (!email || !password) {
-  //    res.status(400).end("<html><body>Bad Request: Please enter email or password!</body></html>\n");
-  //   return;
-  // }
+   if (!email || !password) {
+     res.status(400).end("<html><body>Bad Request: Please enter email or password!</body></html>\n");
+    return;
+  }
   for (var user in users) {
     if (email === users[user].email && bcrypt.compareSync(password, users[user].password)) {
       req.session.userID = users[user].id;
@@ -181,7 +215,11 @@ app.post('/logout', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-  res.render("register");
+  if (checkLoginStatus(req)) {
+    res.redirect('/urls');
+  } else {
+    res.render("register");
+  }
 });
 
 app.post('/register', (req, res) => {
